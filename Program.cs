d087035744ae4ba661038f50b77176f896f112fa
Titoj <Titoj@TITOSLAPTOP>
2022-09-27 13:33:18 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace L5_JRRA_10871321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("José Roberto Rodríguez // 1087121");
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese un número entero");
+            Console.WriteLine("");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Resultado: Positivo");
+            }
+            if (x < 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Resultado: Negativo");
+            }
+            if (x == 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Resultado: Cero");
+            }
+        }
+}
+}
